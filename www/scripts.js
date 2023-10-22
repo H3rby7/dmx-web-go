6065103dc583f0c3500e1676ca8dcd54df35bdca
h3rby7 <herbielutz@gmx.net>
2023-10-23 00:56:02 +0200

    added scene switching example

--- www/scripts.js
+++ www/scripts.js
@@ -55,3 +55,60 @@ function sendSingleDMX(channelElementId, valueElementId) {
       .then(() => feedback("SUCCESS!"))
   }
 }
+
+function fadeMultipleDMX(dmxList, fadeTimeMillis = 1) {
+  const url = `${backendBaseUrl}/dmx/fade`;
+  fetch(url, {
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({ fadeTimeMillis: fadeTimeMillis, scene: {list: dmxList }}),
+    method: "PATCH"
+  })
+    .catch(() => feedback("ERROR!"))
+    .then(() => feedback("SUCCESS!"))
+}
+
+// Channels used for dimming lights
+const dimmerChannels = [1, 2, 3, 4, 8];
+
+const sceneA = {
+  fadeable: [
+    { channel: 1, value: 50 },
+    { channel: 2, value: 100 },
+    { channel: 3, value: 150 },
+  ]
+};
+const sceneB = {
+  fadeable: [
+    { channel: 1, value: 150 },
+    { channel: 4, value: 200 },
+  ]
+};
+const sceneC = {
+  fadeable: [
+    { channel: 8, value: 50 },
+  ],
+  settable: [
+    { channel: 5, value: 255 },
+    { channel: 6, value: 255 },
+    { channel: 7, value: 255 },
+  ]
+};
+
+function switchToScene(scene) {
+  if (scene.settable) {
+    // Send any DMX values that we do not want to fade-in as first request
+    fadeMultipleDMX(scene.settable);
+  }
+  const chMap = new Map();
+  for (let i = 0; i < dimmerChannels.length; i++) {
+    chMap.set(dimmerChannels[i], 0);
+  }
+  for (let i = 0; i < scene.fadeable.length; i++) {
+    const e = scene.fadeable[i];
+    chMap.set(e.channel, e.value);
+  }
+  const dimmerValues = Array.from(chMap, ([channel, value]) => ({ channel, value }));
+  fadeMultipleDMX(dimmerValues, 2000);
+}
