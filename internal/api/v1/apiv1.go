f83b0ea59db179f93db493ab7e3e1f5553a75daf
h3rby7 <herbielutz@gmx.net>
2023-12-08 00:36:28 +0100

    improve logging on API V1 handler registration

--- internal/api/v1/apiv1.go
+++ internal/api/v1/apiv1.go
@@ -10,21 +10,26 @@ import (
 
 // Register Handlers for V1 API
 func RegisterHandlers(g *gin.RouterGroup, services *models_services.ApplicationServices) {
-	log.Debugf("Registering handlers for API V1... ")
+	ll := log.WithField("API", "V1")
+	ll.Debugf("Registering handlers... ")
 	opts := options.GetAppOptions()
 
 	if ok, objection := opts.CanBridge(); ok {
+		ll.Infof("Registering handlers for 'BRIDGE'")
 		apiv1handlers.RegisterBridgeHandlers(g, services.BridgeService)
 	} else {
-		log.Warnf("%s -> Skipping registration of 'bridge' API", objection)
+		ll.Warnf("%s -> Skipping registration of handlers for 'BRIDGE'", objection)
 	}
 
 	if ok, objection := opts.CanWriteDMX(); ok {
+		ll.Infof("Registering handlers for 'DMX'")
 		apiv1handlers.RegisterDMXHandlers(g, services.FadingService)
 	} else {
-		log.Warnf("%s -> Skipping registration of 'DMX' API", objection)
+		ll.Warnf("%s -> Skipping registration of handlers for 'DMX'", objection)
 	}
 
+	ll.Infof("Registering handlers for 'TRIGGER'")
 	apiv1handlers.RegisterTriggerHandlers(g, services.TriggerService)
-	log.Infof("Done registering handlers for API V1")
+
+	ll.Infof("Done registering handlers for API V1")
 }
